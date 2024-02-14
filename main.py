6353642f0bcf5f85241e89cdd18876af6aef55ba
Satyamguru <154254086+Satyamguru@users.noreply.github.com>
2024-02-15 03:39:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ import os
 
 bot = Client(
     "bot",
-    bot_token=os.environ.get("6309028879:AAHLGm1OJlP-sncAO9725BOlqgt77JV8Ya4"),
+    bot_token=os.environ.get("6503744041:AAEDF40jp-DVy71d6Ys3dkjLRZB1GUOm4gs"),
     api_id=int(os.environ.get("15816419")),
     api_hash=os.environ.get("626ed6dab78881858778d9663682962e")
 )
